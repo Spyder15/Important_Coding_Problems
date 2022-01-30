5bb417a1bec8ef81dae5af46ccfedd84be77b07d
priyanshu mundra <97145602+Spyder15@users.noreply.github.com>
2022-01-30 22:31:54 +0530

    Update Two_Sum.cpp
    this is easy and  understanding method

--- LeetCode/Two_Sum.cpp
+++ LeetCode/Two_Sum.cpp
@@ -1,51 +1,23 @@
-//Problem : LeetCode(https://leetcode.com/problems/two-sum/solution/)
-
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
+    vector<int>twoSum(vector<int>& nums, int target) {
+        
+         vector<int>x;
+         int n = nums.size();
+         for(int i = 0;i<n;i++)
+         {
+             for(int j =i+1;j<n;j++)
+             {
+                 if(nums[i]+nums[j]==target)
+                 {
+                     x.push_back(i);
+                     x.push_back(j);   
+                 }
+             }
+         }
+         
+        return x;
     
-      vector<int> twoSum(vector<int>& nums, int target) {
-                map<int,int> m;
-                vector<int> v;
-                int n= nums.size();
-                for(int i=0;i<n;i++)
-                {
-                    
-                        int diff = target - nums[i];
-                        if(m.find(diff) != m.end())
-                        {
-                        auto p = m.find(diff);        
-                        v.push_back(p->second);
-                        v.push_back(i);
-                        }
-                        m.insert(make_pair(nums[i],i));
-                }
-          
-                return v;
-}
+    
+    }
 };
-
-
-int main(){
-    Solution s ;
-    vector<int> v;
-     int n;
-     cin>>n;
-     for(int i=0;i<n;i++)
-     {
-         int element;
-         cin>>element;
-         v.push_back(element);
-     }
-     int target;
-     cin>>target;
-     vector<int> result= s.twoSum (v, target);
-     for(int i: result)
-     {
-          cout<<i<<endl;
-     }
-     return 0;
-
-}
